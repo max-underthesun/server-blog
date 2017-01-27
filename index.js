c6d5b4f550ebecdc4c25bb8046a0b0ac03d71893
max_underthesun <max.underthesun@gmail.com>
2017-01-27 22:38:16 +0400

    add '/posts/:id' route

--- index.js
+++ index.js
@@ -2,10 +2,16 @@ var express = require('express');
 var application = express();
 var cors = require('cors');
 var items = require('./data').items;
+var find = require('lodash').find;
 
 application.use(cors());
 application.get('/', function(req, res) {
   res.json(items);
+});
+application.get('/posts/:id', function(req, res) {
+  var id = req.params.id;
+  var item = find(items, function(item) { return item.id == id; });
+  res.json(item);
 });
 application.listen(3001, function() {
   console.log('Server on 3001');
